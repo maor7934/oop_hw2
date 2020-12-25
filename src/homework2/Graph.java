7f33cf420a34f5a0a925e6d4953c27dc58ddcd4d
nimrod216 <nimrod216@gmail.com>
2020-12-25 11:52:44 +0200

    Graph file - implemention complete, no checkRep

--- src/homework2/Graph.java
+++ src/homework2/Graph.java
@@ -1,10 +1,25 @@
 package homework2;
 
 import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Stream;
 
+class AlreadyContainsException extends Exception{
+
+}
+
+class NotContainsException extends Exception{
+
+}
+
+class EmptyInputException extends Exception{
+
+}
 public class Graph<T> {
+
+    String name;
+    HashMap<String,T> nodes;
+    HashMap<String,Set<String>> children_list;
 
     /**
      * @requires name != Null.
@@ -12,32 +27,55 @@ public class Graph<T> {
      * @return new object of type Graph<T>.
      */
     Graph<T> CreateGraph(String name){
-        ;
+        this.name = name;
+        this.nodes = new HashMap<>();
+        this.children_list = new HashMap<>();
+        return this;
     }
 
     /**
-     *
      * @requires node != Null && name != Null.
      * @modifes add new node to the graph with the name 'name'.
+     * @throws AlreadyContainsException if node name is already inside the graph.
      */
-    void AddNode(T node, String name) {
-
+    void AddNode(T node, String name) throws AlreadyContainsException {
+        this.checkRep();
+        if(this.nodes.containsKey(name)) {
+            throw (new AlreadyContainsException());
+        }
+        this.nodes.put(name,node);
+        this.checkRep();
     }
 
     /**
-     *
      * @requires from != Null && to != Null
      * @modifes add new edge to the graph from node 'from' to the node 'to'.
+     * @throws NotContainsException if 'from' or 'to' are not inside the graph.
+     *         AlreadyContainsException if 'from' already has Edge to 'to'
      */
-    void AddEdge(String from, String to){
-
+    void AddEdge(String from, String to) throws NotContainsException,AlreadyContainsException {
+        this.checkRep();
+        if (!this.nodes.containsKey(from) || !this.nodes.containsKey(to)) {
+            throw (new NotContainsException());
+        }
+        Set<String> children = this.children_list.get(from);
+        if(children.contains(to)){
+            throw (new AlreadyContainsException());
+        }
+        children.add(to);
+        this.children_list.put(from,children);
+        this.checkRep();
     }
 
     /**
      * @effects return the list of names of the nodes in the graph sorted alphabetically
      */
     ArrayList<String> ListNodes(){
-
+        this.checkRep();
+        ArrayList<String> node_names = new ArrayList<>(this.nodes.keySet());
+        Collections.sort(node_names);
+        this.checkRep();
+        return node_names;
     }
 
     /**
@@ -45,21 +83,20 @@ public class Graph<T> {
      * @requires parent_name != Null
      * @effects return the list of names of the nodes that are children of node 'parent_name', sorted alphabetically,
      *          if 'parent_node' has no children return empty list.
-     * @throws InputMismatchException if 'parent_node' not inside the graph.
+     * @throws NotContainsException if 'parent_node' not inside the graph.
      */
-    ArrayList<String> ListChildren(String parent_name){
-
+    ArrayList<String> ListChildren(String parent_name) throws NotContainsException {
+        this.checkRep();
+        if (!this.nodes.containsKey(parent_name)) {
+            throw (new NotContainsException());
+        }
+        ArrayList<String> children_names = new ArrayList<>(this.children_list.get(parent_name));
+        Collections.sort(children_names);
+        this.checkRep();
+        return children_names;
     }
 
-    /**
-     *
-     * @requires from != Null && to != Null
-     * @effects return a list of the nodes that compose the shortest path between one of the nodes in 'from'
-     *          list to one of the nodes in 'to' list. if there is no path - returns empty list.
-     * @throws InputMismatchException if 'from' or 'to' are empty lists.
-     */
-    ArrayList<String> FindPath(List<String> from, List<String> to){
-        ;
-    }
+    void checkRep(){
 
+    }
 }
